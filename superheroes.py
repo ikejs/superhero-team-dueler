0b21593056851dff78094b2ede989903b69987d8
Ike <ikeholzmann@gmail.com>
2019-09-26 17:52:17 -0700

    file name typo fix, add methods: defend, take_damage, is_alive

--- superheroes.py
+++ superheroes.py
@@ -14,7 +14,7 @@ class Armor:
         self.name = name
         self.max_block = max_block
 
-    def block(self, max_block):
+    def block(self):
         return random.randint(0, self.max_block)
 
 
@@ -38,10 +38,32 @@ class Hero:
             attack_strenth =+ self.abilities[ability].attack()
         return attack_strenth
 
+    def defend(self):
+        damage_amt = 0
+        for armor in range(len(self.armors)):
+            damage_amt =+ self.armors[armor].block()
+        return damage_amt
+
+    def take_damage(self, damage):
+        attack = damage-self.defend()
+        if attack < 0:
+            attack = 0 # set attack back to 0 if it goes below 0
+        self.current_health = self.current_health-attack
+
+    def is_alive(self):
+        if self.current_health <= 0:
+            return False
+        return True
+
 
 
 
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    pass
+    hero = Hero("Grace Hopper", 200)
+    hero.take_damage(150)
+    print(hero.is_alive())
+    hero.take_damage(2000)
+    print(hero.current_health)
+    print(hero.is_alive())
---
+++ test.py
@@ -0,0 +1,21 @@
+import superheroes
+
+debug_ability = superheroes.Ability("Debugging Ability", 20)
+assert (0 <= debug_ability.attack() <= 20), "Ability.attack() generated out of range"
+
+debug_ability_1 = superheroes.Ability("Debugging Ability 1", 30)
+
+debug_block = superheroes.Armor("Debug block", 20)
+assert (0 <= debug_block.block() <= 20), "Armor.block() generated out of range"
+
+debug_block_1 = superheroes.Armor("Debug block 1", 30)
+
+test_hero = superheroes.Hero("Grace Hopper", 200)
+test_hero_1 = superheroes.Hero("John Stickler", 150)
+
+test_hero.add_ability(debug_ability)
+test_hero.add_armor(debug_block)
+test_hero_1.add_ability(debug_ability_1)
+test_hero_1.add_armor(debug_block_1)
+
+print(test_hero.name)
